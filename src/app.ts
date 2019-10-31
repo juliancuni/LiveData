d8098ba4e6b6f63fbd181f13b0473f740d1eb2dd
Julian Cuni <juliancuni@gmail.com>
2019-10-31 21:48:17 +0100

    stage Logger middleware

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,14 @@ import bodyparser from 'body-parser';
 
 import { Routes } from './controllers/routes.controller'
 import { MsSqlConnect } from './helpers/mssql';
+import { LoggerMiddleWare } from './middlewares/logger.middleware';
 
 
 const app = express();
 
 app.use(helmet());
 app.use(bodyparser.json());
+app.use(LoggerMiddleWare);
 app.use('/public', express.static('public'))
 // app.use(express.static(process.cwd() + '/public'));
 Routes(app);
---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const LoggerMiddleWare = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const start = new Date().getTime();
+    res.on('finish', () => {
+        const elapsed = new Date().getTime() - start;
+        if(req.originalUrl.includes("public")) {
+        } else {
+            console.info(`IP: ${req.ip} Method: ${req.method} Url: ${req.originalUrl} StatusCode: ${res.statusCode} Time: ${elapsed}ms`)
+        }
+    })
+    next();
+}
+
+export { LoggerMiddleWare };
