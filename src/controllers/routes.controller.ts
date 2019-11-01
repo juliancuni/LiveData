db0ee86762b8e76f1c58c051d90928d245ec381a
Julian Cuni <juliancuni@gmail.com>
2019-11-01 17:44:43 +0100

    stage pilot info, nr i garuesit, kombesia, kategoria etj

--- src/controllers/routes.controller.ts
+++ src/controllers/routes.controller.ts
@@ -19,22 +19,44 @@ export function Routes(app: express.Express) {
         // let dataSotStart = "2018-06-10 00:00:00";
         let dataSotEnd = dataThjeshte + " 23:59:59";
         // let dataSotEnd = "2018-06-10 23:59:59";
-        let testQry = "SELECT tavl.Object.Number as Mjet,m.X as Lng,m.Y as Lat  FROM tavl.Message as m  JOIN ( SELECT tavl.Message.ObjectId, MAX(MessageId) as MessageId FROM tavl.Message inner join tavl.Object as o on tavl.Message.ObjectId=o.ObjectId where o.ClientId=16 and o.Number!='null' and GpsTime >= '" + dataSotStart + "' and GpsTime <= '" + dataSotEnd + "' GROUP BY tavl.Message.ObjectId ) t2 ON m.ObjectId = t2.ObjectId AND m.MessageId = t2.MessageId join tavl.Object on tavl.Object.ObjectId=m.ObjectId";
-        ExecQuery(testQry).then((data) => {
-            // console.log(data);
-            let mjetetSort = data.sort(function (a, b) {
-                var mjetiA = a.Mjet.toUpperCase(); 
-                var mjetiB = b.Mjet.toUpperCase(); 
-                if (mjetiA < mjetiB) {
-                    return -1;
+        let dataQry = "SELECT tavl.Object.Comment as Mjet, tavl.Object.Number as nrGaruesit, m.X as Lng,m.Y as Lat  FROM tavl.Message as m  JOIN ( SELECT tavl.Message.ObjectId, MAX(MessageId) as MessageId FROM tavl.Message inner join tavl.Object as o on tavl.Message.ObjectId=o.ObjectId where o.ClientId=16 and o.Number!='null' and GpsTime >= '" + dataSotStart + "' and GpsTime <= '" + dataSotEnd + "' GROUP BY tavl.Message.ObjectId ) t2 ON m.ObjectId = t2.ObjectId AND m.MessageId = t2.MessageId join tavl.Object on tavl.Object.ObjectId=m.ObjectId";
+        ExecQuery(dataQry).then((data) => {
+            // let mjetetSort = data.sort(function (a, b) {
+            //     var mjetiA = a.Mjet.toUpperCase(); 
+            //     var mjetiB = b.Mjet.toUpperCase(); 
+            //     if (mjetiA < mjetiB) {
+            //         return -1;
+            //     }
+            //     if (mjetiA > mjetiB) {
+            //         return 1;
+            //     }
+            //     return 0;
+            // })
+            // res.render('../views/index.ejs', { mjetet: mjetetSort });
+            let listaAsistenca: any[] = [];
+            let listaGaruesve: any[] = [];
+            data.forEach((dt) => {
+                // console.log(dt.nrGaruesit)
+                let dataSplit = dt.Mjet.split('|')
+                dt.emer = dataSplit[0];
+                dt.kombesia = dataSplit[2].toUpperCase();
+                dt.kategoria = dataSplit[3];
+                if (dataSplit[1].toLowerCase().includes('gps')) {
+                    dt.neGare = true;
+                } else {
+                    dt.neGare = false;
                 }
-                if (mjetiA > mjetiB) {
-                    return 1;
-                }
-                return 0;
             })
-            res.render('../views/index.ejs', { mjetet: mjetetSort });
-            // res.render("../views/test", { Mjet: data });
+
+            listaAsistenca = data.filter((dt) => {
+                return !dt.neGare;
+            })
+
+            listaGaruesve = data.filter((dt) => {
+                return dt.neGare;
+            })
+
+            res.render("../views/index.ejs", { garuesit: listaGaruesve, asistenca: listaAsistenca });
         }).catch((err) => {
             console.log(err);
             res.sendStatus(500);
@@ -58,8 +80,8 @@ export function Routes(app: express.Express) {
         // let dataSotStart = "2018-06-10 00:00:00";
         let dataSotEnd = dataThjeshte + " 23:59:59";
         // let dataSotEnd = "2018-06-10 23:59:59";
-        let testQry = "SELECT tavl.Object.Number as Mjet,m.X as Lng,m.Y as Lat FROM tavl.Message as m JOIN ( SELECT tavl.Message.ObjectId, MAX(MessageId) as MessageId FROM tavl.Message inner join tavl.Object as o on tavl.Message.ObjectId=o.ObjectId where o.ClientId=16 and o.Number!='null' and GpsTime >= '" + dataSotStart + "' and GpsTime <= '" + dataSotEnd + "' GROUP BY tavl.Message.ObjectId ) t2 ON m.ObjectId = t2.ObjectId AND m.MessageId = t2.MessageId join tavl.Object on tavl.Object.ObjectId=m.ObjectId";
-        ExecQuery(testQry).then((data) => {
+        let dataQry = "SELECT tavl.Object.Number as Mjet,m.X as Lng,m.Y as Lat FROM tavl.Message as m JOIN ( SELECT tavl.Message.ObjectId, MAX(MessageId) as MessageId FROM tavl.Message inner join tavl.Object as o on tavl.Message.ObjectId=o.ObjectId where o.ClientId=16 and o.Number!='null' and GpsTime >= '" + dataSotStart + "' and GpsTime <= '" + dataSotEnd + "' GROUP BY tavl.Message.ObjectId ) t2 ON m.ObjectId = t2.ObjectId AND m.MessageId = t2.MessageId join tavl.Object on tavl.Object.ObjectId=m.ObjectId";
+        ExecQuery(dataQry).then((data) => {
             res.json(data);
         }).catch((err) => {
             console.log(err);
--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -5,6 +5,9 @@ const LoggerMiddleWare = (req: express.Request, res: express.Response, next: exp
     res.on('finish', () => {
         const elapsed = new Date().getTime() - start;
         let remoteIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        let dateTime = new Date().toLocaleString("sq-AL", {timeZone: "Europe/Tirane"})
+
+        // console.log(dateTime)
         // if(!req.originalUrl.includes("public")) console.info(`IP: ${req.ip} Method: ${req.method} Url: ${req.originalUrl} StatusCode: ${res.statusCode} Time: ${elapsed}ms`)
         if(!req.originalUrl.includes("public")) console.info(`IP: ${remoteIp} Method: ${req.method} Url: ${req.originalUrl} StatusCode: ${res.statusCode} Time: ${elapsed}ms`)
     })
