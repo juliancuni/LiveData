598d04c70b6f374a4015844845f6def6fc20ba0c
Julian Cuni <juliancuni@gmail.com>
2019-11-01 10:35:09 +0100

    stage middleware_log remote ip address

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -4,7 +4,9 @@ const LoggerMiddleWare = (req: express.Request, res: express.Response, next: exp
     const start = new Date().getTime();
     res.on('finish', () => {
         const elapsed = new Date().getTime() - start;
-        if(!req.originalUrl.includes("public")) console.info(`IP: ${req.ip} Method: ${req.method} Url: ${req.originalUrl} StatusCode: ${res.statusCode} Time: ${elapsed}ms`)
+        let remoteIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        // if(!req.originalUrl.includes("public")) console.info(`IP: ${req.ip} Method: ${req.method} Url: ${req.originalUrl} StatusCode: ${res.statusCode} Time: ${elapsed}ms`)
+        if(!req.originalUrl.includes("public")) console.info(`IP: ${remoteIp} Method: ${req.method} Url: ${req.originalUrl} StatusCode: ${res.statusCode} Time: ${elapsed}ms`)
     })
     next();
 }
