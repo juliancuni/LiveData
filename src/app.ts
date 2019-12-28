a08b094580f6871a9c3648675d7a6f8edf316cdf
Julian Cuni <juliancuni@gmail.com>
2019-12-28 11:49:43 +0100

    DummyData per Lulen

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import helmet from 'helmet';
 import bodyparser from 'body-parser';
 
 import { Routes } from './controllers/routes.controller'
-import { MsSqlConnect } from './helpers/mssql';
+// import { MsSqlConnect } from './helpers/mssql';
 import { LoggerMiddleWare } from './middlewares/logger.middleware';
 
 
@@ -16,33 +16,33 @@ app.use(LoggerMiddleWare);
 app.use('/public', express.static('public'))
 Routes(app);
 const server: http.Server = app.listen(3000);
-MsSqlConnect();
+// MsSqlConnect();
 server.on("listening", () => { console.info("Live_Data running") });
 
 
-import * as net from 'net';
+// import * as net from 'net';
 
-const host = "172.24.0.24";
-const port = 6608;
+// const host = "172.24.0.24";
+// const port = 6608;
 
-let onClientConnected = (sock: net.Socket) => {
-    let remoteAddress = sock.remoteAddress + ':' + sock.remotePort;
-    console.log('client connected %s', remoteAddress);
-    sock.on('data', (data) => {
-        console.log('Client dergoi: ' + data.toString())
-        // console.log('%s dergoi %s', remoteAddress, data);
-        sock.write('exit');
-    })
-    sock.on('close', () => {
-        console.log('connection from %s closed', remoteAddress);
-    })
-    sock.on('error', (err) => {
-        console.log('Conn %s error %s', remoteAddress, err.message);
-    })
-}
+// let onClientConnected = (sock: net.Socket) => {
+//     let remoteAddress = sock.remoteAddress + ':' + sock.remotePort;
+//     console.log('client connected %s', remoteAddress);
+//     sock.on('data', (data) => {
+//         console.log('Client dergoi: ' + data.toString())
+//         // console.log('%s dergoi %s', remoteAddress, data);
+//         sock.write('exit');
+//     })
+//     sock.on('close', () => {
+//         console.log('connection from %s closed', remoteAddress);
+//     })
+//     sock.on('error', (err) => {
+//         console.log('Conn %s error %s', remoteAddress, err.message);
+//     })
+// }
 
-const tcpServer = net.createServer(onClientConnected);
+// const tcpServer = net.createServer(onClientConnected);
 
-tcpServer.listen(port, host, () => {
-    console.log('Listening on %j', tcpServer.address());
-})
+// tcpServer.listen(port, host, () => {
+//     console.log('Listening on %j', tcpServer.address());
+// })
--- src/controllers/routes.controller.ts
+++ src/controllers/routes.controller.ts
@@ -13,47 +13,65 @@ export function Routes(app: express.Express) {
     })
 
     app.get('/', (req: express.Request, res: express.Response) => {
-
-        const dataSot = new Date();
-        const dataThjeshte = dataSot.getFullYear().toString() + '-' + (dataSot.getMonth() + 1).toString() + '-' + dataSot.getDate().toString();
-        let dataSotStart = dataThjeshte + " 00:00:00";
-        // let dataSotStart = "2018-06-10 00:00:00";
-        let dataSotEnd = dataThjeshte + " 23:59:59";
-        // let dataSotEnd = "2018-06-10 23:59:59";
-        let dataQry = "SELECT tavl.Object.Comment as bruto, tavl.Object.Number as nrGaruesit, m.X as Lng,m.Y as Lat  FROM tavl.Message as m  JOIN ( SELECT tavl.Message.ObjectId, MAX(MessageId) as MessageId FROM tavl.Message inner join tavl.Object as o on tavl.Message.ObjectId=o.ObjectId where o.ClientId=16 and o.Number!='null' and GpsTime >= '" + dataSotStart + "' and GpsTime <= '" + dataSotEnd + "' GROUP BY tavl.Message.ObjectId ) t2 ON m.ObjectId = t2.ObjectId AND m.MessageId = t2.MessageId join tavl.Object on tavl.Object.ObjectId=m.ObjectId";
-        ExecQuery(dataQry).then((data) => {
-            let listaAsistenca: any[] = [];
-            let listaGaruesve: any[] = [];
-            data.forEach((dt) => {
-                // console.log(dt.nrGaruesit)
-                let dataSplit = dt.bruto.split('|')
-                dt.emer = dataSplit[0];
-                dt.kombesia = dataSplit[2].toUpperCase();
-                dt.kategoria = dataSplit[3];
-                if (dataSplit[1].toLowerCase().includes('gps')) {
-                    dt.neGare = true;
-                } else {
-                    dt.neGare = false;
-                }
-            })
-
-            listaAsistenca = data.filter((dt) => {
-                return !dt.neGare;
-            })
-
-            listaGaruesve = data.filter((dt) => {
-                return dt.neGare;
-            })
-
-            res.render("../views/index.ejs", { garuesit: listaGaruesve, asistenca: listaAsistenca });
-        }).catch((err) => {
-            console.log(err);
-            res.sendStatus(500);
-        })
+        let listaAsistenca: any[] = [
+            { emer: "Safety Car", kombesia: "AL", kategoria: "C1", nrGaruesit: "007", neGare: false, Lat: 41.3451283, Lng: 19.771245 },
+            { emer: "Idi ", kombesia: "AL", kategoria: "C1", nrGaruesit: "120", neGare: false, Lat: 41.354409, Lng: 19.7721059 }
+        ];
+        let listaGaruesve: any[] = [
+            { emer: "Dorian Nurka", kombesia: "AL", kategoria: "C1", nrGaruesit: 301, neGare: true, Lat: 41.3037934, Lng: 19.8095346 },
+            { emer: "Almir Dervishaj", kombesia: "AL", kategoria: "C1", nrGaruesit: 385, neGare: true, Lat: 41.3506832, Lng: 19.7475187 },
+            { emer: "Ermal Pashaj", kombesia: "AL", kategoria: "M1", nrGaruesit: 908, neGare: true, Lat: 41.352081, Lng: 19.745073 }
+            
+        ];
+        res.render("../views/index.ejs", { garuesit: listaGaruesve, asistenca: listaAsistenca });
     });
 
-    app.get('/html', (req: express.Request, res: express.Response) => {
-        res.sendFile(path.join(__dirname + '/index.html'));
+    // app.get('/', (req: express.Request, res: express.Response) => {
+
+    //     const dataSot = new Date();
+    //     const dataThjeshte = dataSot.getFullYear().toString() + '-' + (dataSot.getMonth() + 1).toString() + '-' + dataSot.getDate().toString();
+    //     let dataSotStart = dataThjeshte + " 00:00:00";
+    //     // let dataSotStart = "2018-06-10 00:00:00";
+    //     let dataSotEnd = dataThjeshte + " 23:59:59";
+    //     // let dataSotEnd = "2018-06-10 23:59:59";
+    //     let dataQry = "SELECT tavl.Object.Comment as bruto, tavl.Object.Number as nrGaruesit, m.X as Lng,m.Y as Lat  FROM tavl.Message as m  JOIN ( SELECT tavl.Message.ObjectId, MAX(MessageId) as MessageId FROM tavl.Message inner join tavl.Object as o on tavl.Message.ObjectId=o.ObjectId where o.ClientId=16 and o.Number!='null' and GpsTime >= '" + dataSotStart + "' and GpsTime <= '" + dataSotEnd + "' GROUP BY tavl.Message.ObjectId ) t2 ON m.ObjectId = t2.ObjectId AND m.MessageId = t2.MessageId join tavl.Object on tavl.Object.ObjectId=m.ObjectId";
+    //     ExecQuery(dataQry).then((data) => {
+    //         let listaAsistenca: any[] = [];
+    //         let listaGaruesve: any[] = [];
+    //         data.forEach((dt) => {
+    //             // console.log(dt.nrGaruesit)
+    //             let dataSplit = dt.bruto.split('|')
+    //             dt.emer = dataSplit[0];
+    //             dt.kombesia = dataSplit[2].toUpperCase();
+    //             dt.kategoria = dataSplit[3];
+    //             if (dataSplit[1].toLowerCase().includes('gps')) {
+    //                 dt.neGare = true;
+    //             } else {
+    //                 dt.neGare = false;
+    //             }
+    //         })
+
+    //         listaAsistenca = data.filter((dt) => {
+    //             return !dt.neGare;
+    //         })
+
+    //         listaGaruesve = data.filter((dt) => {
+    //             return dt.neGare;
+    //         })
+
+    //         res.render("../views/index.ejs", { garuesit: listaGaruesve, asistenca: listaAsistenca });
+    //     }).catch((err) => {
+    //         console.log(err);
+    //         res.sendStatus(500);
+    //     })
+    // });
+
+    app.get('/leaflet', (req: express.Request, res: express.Response) => {
+        res.sendFile(path.join(__dirname + '/leaflet.html'));
+    })
+
+    app.get('/gmaps', (req: express.Request, res: express.Response) => {
+        res.sendFile(path.join(__dirname + '/gmaps.html'));
     })
 
     app.get('/api/mjetetgps', (req: express.Request, res: express.Response) => {
